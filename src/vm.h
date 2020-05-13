4ee7b0ca10573f04a3c2e1cda27c50353e48d64e
hogsy <markelswo@gmail.com>
2020-05-13 17:20:17 +0100

    cont. on virtual machine spec

--- src/vm.h
+++ src/vm.h
@@ -4,8 +4,20 @@
 
 #pragma once
 
+/*--------------------------
+ * CVM Specification
+ * */
+
+#define VM_BINARY_EXTENSION ".cvm"
+#define VM_BINARY_VERSION   20200506
+
+/*--------------------------
+ * VM API
+ * */
+
 typedef struct VMProgram VMProgram;
 
+VMProgram *VM_GetProgramByName( const char *programName );
 VMProgram *VM_LoadCVM( const char *path, size_t memoryPoolSize );
 void VM_Tick( void );
 void VM_Initialize( void );
