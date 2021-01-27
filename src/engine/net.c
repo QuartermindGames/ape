a22ba3aa31d11478276585cbbdaee979568f6986
hogsy <markelswo@gmail.com>
2021-01-27 11:14:05 +0000

    Start on networking impl.

---
+++ src/engine/net.c
@@ -0,0 +1,28 @@
+/* ======================================================================
+ * Project Yin, Confidential
+ * Copyright (C) 2020-2021 Mark E Sowden <hogsy@oldtimes-software.com>
+ * ====================================================================*/
+
+#include <sys/socket.h>
+
+#include "yin.h"
+#include "net.h"
+
+#define NET_DEFAULT_ADDRESS 127.0.0.1
+#define NET_DEFAULT_PORT    8080
+
+int serverSocket = 0;
+
+/* setup a server */
+void Net_SetupServer( void ) {
+	serverSocket = socket( AF_INET, SOCK_STREAM, 0 );
+	if ( serverSocket == 0 ) {
+		PrintWarn( "Failed to create socket!\n" );
+		return;
+	}
+}
+
+/* connect to the server */
+void Net_ConnectServer( void ) {
+
+}
