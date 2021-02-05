b30db82865749386c6529fc4ffc64d9089244b05
hogsy <markelswo@gmail.com>
2021-02-05 16:58:26 +0000

    Implemented safer way of fetching vars inc. caching.

--- src/common/include/common/common.h
+++ src/common/include/common/common.h
@@ -9,11 +9,20 @@
 
 #if !defined( __bool_true_false_are_defined )
 typedef unsigned char bool;
-enum { false, true };
+enum { false,
+	   true };
 #endif
 
 #if defined( COMMON_DLL )
-#   define COMMON_API PL_EXPORT
+#define COMMON_API PL_EXPORT
 #else
-#   define COMMON_API PL_IMPORT
+#define COMMON_API PL_IMPORT
 #endif
+
+#define CVar( NAME, STORE )                           \
+	static PLConsoleVariable *( STORE ) = NULL;       \
+	if ( ( STORE ) == NULL ) {                        \
+		( STORE ) = plGetConsoleVariable( ( NAME ) ); \
+		u_assert( ( STORE ) != NULL );                \
+	}                                                 \
+	( STORE )
