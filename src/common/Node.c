56af2bd352ddac92106d2e347eb89f503645ae6a
hogsy <markelswo@gmail.com>
2021-04-18 20:55:16 +0100

    Parser now works (mostly) for node files, yay!

--- src/common/Node.c
+++ src/common/Node.c
@@ -40,7 +40,10 @@ static void NL_ClearErrorMessage( void ) {
 	nlErrorMsg = NULL;
 	nlErrorType = NL_ERROR_SUCCESS;
 }
+
 static void NL_SetErrorMessage( NLErrorCode type, const char *msg, ... ) {
+	NL_ClearErrorMessage();
+
 	nlErrorType = type;
 
 	va_list args;
@@ -289,9 +292,9 @@ NLNode *NL_PushBackBool( NLNode *parent, const char *name, bool var ) {
 NLNode *NL_PushBackInt( NLNode *parent, const char *name, int var ) {
 	NLNode *node = xNL_PushBackNode( parent, name, NODE_PROPERTY_INTEGER );
 	if ( node != NULL ) {
-        char buf[ 32 ];
-        snprintf( buf, sizeof( buf ), "%d", var );
-        node->data.strBuf = AllocVarString( buf, &node->data.strBufLength );
+		char buf[ 32 ];
+		snprintf( buf, sizeof( buf ), "%d", var );
+		node->data.strBuf = AllocVarString( buf, &node->data.strBufLength );
 	}
 	return node;
 }
@@ -307,20 +310,20 @@ NLNode *NL_PushBackFloat( NLNode *parent, const char *name, float var ) {
 }
 
 NLNode *NL_PushBackIntArray( NLNode *parent, const char *name, const int *array, unsigned int numElements ) {
-    NLNode *node = xNL_PushBackNode( parent, name, NODE_PROPERTY_ARRAY );
-    if ( node != NULL ) {
+	NLNode *node = xNL_PushBackNode( parent, name, NODE_PROPERTY_ARRAY );
+	if ( node != NULL ) {
 		node->childType = NODE_PROPERTY_INTEGER;
-        for ( unsigned int i = 0; i < numElements; ++i ) {
-            NL_PushBackInt( node, NULL, array[ i ] );
-        }
-    }
-    return node;
+		for ( unsigned int i = 0; i < numElements; ++i ) {
+			NL_PushBackInt( node, NULL, array[ i ] );
+		}
+	}
+	return node;
 }
 
 NLNode *NL_PushBackFloatArray( NLNode *parent, const char *name, const float *array, unsigned int numElements ) {
 	NLNode *node = xNL_PushBackNode( parent, name, NODE_PROPERTY_ARRAY );
 	if ( node != NULL ) {
-        node->childType = NODE_PROPERTY_FLOAT;
+		node->childType = NODE_PROPERTY_FLOAT;
 		for ( unsigned int i = 0; i < numElements; ++i ) {
 			NL_PushBackFloat( node, NULL, array[ i ] );
 		}
@@ -329,15 +332,15 @@ NLNode *NL_PushBackFloatArray( NLNode *parent, const char *name, const float *ar
 }
 
 NLNode *NL_PushBackVec2( NLNode *parent, const char *name, const PLVector2 *var ) {
-	return NL_PushBackFloatArray( parent, name, ( float* ) var, 2 );
+	return NL_PushBackFloatArray( parent, name, ( float * ) var, 2 );
 }
 
 NLNode *NL_PushBackVec3( NLNode *parent, const char *name, const PLVector3 *var ) {
-	return NL_PushBackFloatArray( parent, name, ( float* ) var, 3 );
+	return NL_PushBackFloatArray( parent, name, ( float * ) var, 3 );
 }
 
 NLNode *NL_PushBackVec4( NLNode *parent, const char *name, const PLVector4 *var ) {
-	return NL_PushBackFloatArray( parent, name, ( float* ) var, 4 );
+	return NL_PushBackFloatArray( parent, name, ( float * ) var, 4 );
 }
 
 void NL_DestroyNode( NLNode *node ) {
@@ -461,7 +464,7 @@ NLNode *NL_LoadFile( const char *path, const char *objectType ) {
 	} else if ( fileType == NL_FILE_ASCII ) {
 		/* first need to run the pre-processor on it */
 		size_t length = plGetFileSize( file );
-        const char *data = ( const char* ) plGetFileData( file ) + strlen( NL_ASCII_HEADER );
+		const char *data = ( const char * ) plGetFileData( file ) + strlen( NL_ASCII_HEADER );
 		char *buf = pl_malloc( length );
 		memcpy( buf, data, length );
 
@@ -494,7 +497,17 @@ NLNode *NL_LoadFile( const char *path, const char *objectType ) {
 /******************************************/
 /** Serialisation **/
 
-void SerializeStringVar( const NLVarString *string, NLFileType fileType, FILE *file ) {
+static unsigned int sDepth; /* serialisation depth */
+static void TabifyLine( FILE *file, const char *string ) {
+	for ( unsigned int i = 0; i < sDepth; ++i ) fputc( '\t', file );
+	if ( string == NULL ) {
+		return;
+	}
+
+	fprintf( file, "%s", string );
+}
+
+static void SerializeStringVar( const NLVarString *string, NLFileType fileType, FILE *file ) {
 	if ( fileType == NL_FILE_BINARY ) {
 		fwrite( &string->strBufLength, sizeof( uint32_t ), 1, file );
 		/* slightly paranoid here, because strBuf is probably null if length is 0
@@ -528,9 +541,9 @@ void SerializeStringVar( const NLVarString *string, NLFileType fileType, FILE *f
 	}
 
 	if ( encloseString ) {
-		fprintf( file, "\"%s\"", string->strBuf );
+		fprintf( file, "\"%s\" ", string->strBuf );
 	} else {
-		fprintf( file, "%s", string->strBuf );
+		fprintf( file, "%s ", string->strBuf );
 	}
 }
 
@@ -538,14 +551,16 @@ static void SerializeNodeTree( FILE *file, NLNode *root, NLFileType fileType );
 static void SerializeNode( FILE *file, NLNode *node, NLFileType fileType ) {
 	if ( fileType == NL_FILE_ASCII ) {
 		/* write out the line identifying this node */
+		TabifyLine( file, NULL );
 		fprintf( file, "%s ", StringForPropertyType( node->type ) );
 		SerializeStringVar( &node->name, fileType, file );
-		fprintf( file, " " );
 		/* if this node has children, serialize all those */
 		if ( node->type == NODE_PROPERTY_OBJECT || node->type == NODE_PROPERTY_ARRAY ) {
-			fprintf( file, "{\n" );
+            TabifyLine( file, "{\n" );
+			sDepth++;
 			SerializeNodeTree( file, node, fileType );
-			fprintf( file, "}\n\n" );
+            sDepth--;
+            TabifyLine( file, "}\n\n" );
 		} else {
 			SerializeStringVar( &node->data, fileType, file );
 			fprintf( file, "\n" );
@@ -608,7 +623,8 @@ void NL_WriteFile( const char *path, NLNode *root, NLFileType fileType ) {
 	if ( fileType == NL_FILE_BINARY ) {
 		fprintf( file, NL_BINARY_HEADER "\n" );
 	} else {
-		fprintf( file, NL_ASCII_HEADER "\n" );
+        sDepth = 0;
+		fprintf( file, NL_ASCII_HEADER "\n; this node file has been auto-generated!\n" );
 	}
 
 	SerializeNode( file, root, fileType );
@@ -638,7 +654,7 @@ void NL_PrintNodeTree( NLNode *node, int index ) {
 	} else {
 		NLNode *parent = NL_GetParent( node );
 		if ( parent != NULL && parent->type == NODE_PROPERTY_ARRAY ) {
-            Message( "%s %s\n", StringForPropertyType( node->type ), node->data.strBuf );
+			Message( "%s %s\n", StringForPropertyType( node->type ), node->data.strBuf );
 		} else {
 			Message( "%s %s %s\n", StringForPropertyType( node->type ), node->name.strBuf, node->data.strBuf );
 		}
