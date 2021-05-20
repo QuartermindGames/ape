678a177c594011f5fb4052332b92ff5ec3c66dd8
hogsy <markelswo@gmail.com>
2021-05-20 18:50:56 +0100

    Remove lightmapper file, meh.

--- src/OSCommonFramework/CFWLightMapper.c
+++
@@ -1,7 +0,0 @@
-/* ======================================================================
- * Project Yin, Confidential
- * Copyright (C) 2020-2021 Mark E Sowden <hogsy@oldtimes-software.com>
- * ====================================================================*/
-
-#include <plcore/pl_linkedlist.h>
-
