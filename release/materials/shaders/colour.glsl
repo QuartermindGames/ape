113fcdd58e8d42a588d70aefdbd224500e79c223
hogsy <markelswo@gmail.com>
2021-08-19 13:43:14 +0100

    Splat.

--- release/materials/shaders/colour.glsl
+++
@@ -1,5 +0,0 @@
-in vec4 vsColour;
-
-void main() {
-    pl_frag = vsColour;
-}
--- release/materials/shaders/texture.glsl
+++
@@ -1,8 +0,0 @@
-uniform sampler2D diffuseMap;
-
-in vec2 vsUV;
-in vec4 vsColour;
-
-void main() {
-    pl_frag = vsColour * texture(diffuseMap, vsUV);
-}
