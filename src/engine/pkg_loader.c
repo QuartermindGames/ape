4572038d9239f97fc14bba2989e1add67dc0c842
hogsy <markelswo@gmail.com>
2021-09-04 14:35:20 +0100

    Use the generic loader the package library provides.

--- src/engine/pkg_loader.c
+++ src/engine/pkg_loader.c
@@ -8,40 +8,6 @@
 #include "pkg_loader.h"
 #include "miniz.h"
 
-uint8_t *Pkg_OpenFile( PLFile *file, PLPackageIndex *index )
-{
-	if ( !PlFileSeek( file, ( signed ) index->offset, PL_SEEK_SET ) )
-		return NULL;
-
-	uint8_t *data = globalSystem.MAlloc( index->compressedSize, true );
-	if ( PlReadFile( file, data, index->compressedSize, 1 ) != 1 )
-	{
-		globalSystem.Free( data );
-		return NULL;
-	}
-
-	if ( index->compressionType == PL_COMPRESSION_ZLIB )
-	{
-		/* go ahead and decompress it */
-		uint8_t *	  uncompressedData = globalSystem.MAlloc( index->fileSize, true );
-		unsigned long uncompressedLength;
-		int			  status = mz_uncompress( uncompressedData, &uncompressedLength, data, ( mz_ulong ) index->compressedSize );
-
-		/* don't need this anymore! */
-		globalSystem.Free( data );
-		data = uncompressedData;
-
-		if ( status != MZ_OK )
-		{
-			globalSystem.Free( uncompressedData );
-			PrintWarn( "Failed to decompress \"%s\" from package \"%s\"!\n", index->fileName, PlGetFilePath( file ) );
-			return NULL;
-		}
-	}
-
-	return data;
-}
-
 PLPackage *Pkg_LoadPackage( const char *path )
 {
 	PLFile *filePtr = PlOpenFile( path, false );
@@ -69,7 +35,7 @@ PLPackage *Pkg_LoadPackage( const char *path )
 	if ( !status )
 		PrintError( "Failed to read in the number of files within the \"%s\" package!\nPL: %s\n", path, PlGetError() );
 
-	PLPackage *package = PlCreatePackageHandle( path, numFiles, Pkg_OpenFile );
+	PLPackage *package = PlCreatePackageHandle( path, numFiles, NULL );
 	for ( unsigned int i = 0; i < numFiles; ++i )
 	{
 		PLPackageIndex *index = &package->table[ i ];
