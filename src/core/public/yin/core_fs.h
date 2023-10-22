14fd1fd8b30aa57e5f58c553d836a46bc0d686fb
hogsy <hogsy@oldtimes-software.com>
2023-10-22 20:38:51 +0100

    more parse functions, yay

--- src/core/public/yin/core_fs.h
+++ src/core/public/yin/core_fs.h
@@ -16,13 +16,22 @@ void acl_fs_mount_base_locations( void );
 
 char *acl_fs_parse_string( PLFile *file, uint16_t *size );
 char *acl_fs_parse_string_ex( PLFile *file, uint16_t *size, unsigned int version, unsigned int minVersion, unsigned int maxVersion );
+
+uint8_t acl_fs_parse_byte( PLFile *file );
+uint8_t acl_fs_parse_byte_ex( PLFile *file, unsigned int version, unsigned int minVersion, unsigned int maxVersion, uint8_t fallback );
+
 PLColour acl_fs_parse_colour( PLFile *file );
 PLMatrix3 acl_fs_parse_mat3( PLFile *file );
+
 PLVector3 acl_fs_parse_vector( PLFile *file );
+PLVector3 acl_fs_parse_vector_ex( PLFile *file, unsigned int version, unsigned int minVersion, unsigned int maxVersion, const PLVector3 *fallback );
+
 PLVector4 acl_fs_parse_vector4( PLFile *file );
 PLVector4 acl_fs_parse_vector4_ex( PLFile *file, unsigned int version, unsigned int minVersion, unsigned int maxVersion, const PLVector4 *fallback );
+
 int acl_fs_parse_int( PLFile *file );
 int acl_fs_parse_int_ex( PLFile *file, unsigned int version, unsigned int minVersion, unsigned int maxVersion, int fallback );
+
 float acl_fs_parse_float( PLFile *file );
 float acl_fs_parse_float_ex( PLFile *file, unsigned int version, unsigned int minVersion, unsigned int maxVersion, float fallback );
 
