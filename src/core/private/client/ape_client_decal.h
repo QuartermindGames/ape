9ee631eb5e462a45594534974d01399a4a5c95cb
Mark Sowden <hogsy@oldtimes-software.com>
2023-12-07 21:39:19 +0000

    No need kicking decal code around - removed.

--- src/core/private/client/ape_client_decal.h
+++
@@ -1,8 +0,0 @@
-// Copyright © 2020-2023 OldTimes Software, Mark E Sowden <hogsy@oldtimes-software.com>
-
-#pragma once
-
-typedef struct ApeMaterial ApeMaterial;
-typedef struct ApeDecal ApeDecal;
-
-ApeDecal *apeCreateDecal( ApeMaterial *material );
