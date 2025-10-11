a08e4577a146c1051bcdcec19bf3efb7ba3936f9
hogsy <hogsy@oldtimes-software.com>
2025-10-11 19:05:49 +0100

    game: further improvements to the compass
    Signed-off-by: hogsy <hogsy@oldtimes-software.com>

--- src/game/private/shared/menu/menu_compass.c
+++ src/game/private/shared/menu/menu_compass.c
@@ -6,16 +6,20 @@
 
 static QmMathVector3f compassAngles;
 
-static ApeGuiFont *compassFont;
+static ApeGuiFont  *compassFont;
+static ApeMaterial *compassBackground;
 
 void game_menu_compass_initialize_( ApeGuiFont *font )
 {
 	compassAngles = QM_MATH_VECTOR3F_ZERO;
 	compassFont   = font;
+
+	compassBackground = ape_material_cache( "materials/ui/ui_circle.mat.n", APE_CACHE_GROUP_GLOBAL, true );
 }
 
 void game_menu_compass_shutdown_()
 {
+	ape_material_release( compassBackground );
 }
 
 void game_menu_compass_draw_( const ApeViewport *viewport )
@@ -23,11 +27,14 @@ void game_menu_compass_draw_( const ApeViewport *viewport )
 	ApeMaterial *material = ape_material_get_default( APE_MATERIAL_DEFAULT_VERTEX );
 	assert( material != nullptr );
 
-	float x     = viewport->width / 2.0f;
-	float y     = 128.0f;
-	float w     = viewport->width / 4.0f;
-	float h     = 128.0f;
-	float scale = 1.0f;
+	float scale     = 1.0f;
+	const float w = 256.0f * scale;
+	const float h = 128.0f * scale;
+
+	const float x = w / 2.0f + 32.0f;
+	const float y = viewport->height - w / 2.0f + 32.0f;
+
+	ape_draw_textured_quad( compassBackground, x - w / 2.0f, y - h / 2.0f, w * scale, h * scale, &QM_MATH_COLOUR4UB( 0, 0, 0, 128 ), 0.0f );
 
 	PLGMesh *mesh = PlgImmBegin( PLG_MESH_LINES );
 
@@ -36,34 +43,42 @@ void game_menu_compass_draw_( const ApeViewport *viewport )
 		float xo = x + cosf( PL_DEG2RAD( ( float ) i + compassAngles.y ) ) * ( w / 2.0f * scale );
 		float yo = y + sinf( PL_DEG2RAD( ( float ) i + compassAngles.y ) ) * ( h / 2.0f * scale );
 
-		float height = i % 3 ? 2.0f : 16.0f;
+		float vpos = ( yo - y ) / ( h / 2.0f );
+		if ( vpos >= 0.0f )
+		{
+			continue;
+		}
 
-		//float height = 8.0f;
+		float height = ( i % 3 ? 2.0f : 16.0f );
 
 		PlgImmPushVertex( xo, yo, 0.0f );
 		PlgImmColour( 255, 255, 255, 255 );
-		PlgImmPushVertex( xo, yo - height * scale, 0.0f );
+		PlgImmPushVertex( xo, yo - height * ( -vpos * scale ), 0.0f );
 		PlgImmColour( 255, 255, 255, 255 );
 	}
 
 	static constexpr char CHARS[ 4 ] = { 'N', 'E', 'S', 'W' };
 
 	const char *c = CHARS;
-	for ( unsigned int i = 0; i < 360; i += 360 / 4 )
+	for ( unsigned int i = 0; i < 360; i += 360 / 4, c++ )
 	{
 		float xo = x + cosf( PL_DEG2RAD( ( float ) i + compassAngles.y ) ) * ( w / 2.0f * scale );
 		float yo = y + sinf( PL_DEG2RAD( ( float ) i + compassAngles.y ) ) * ( h / 2.0f * scale );
 
-		float xc = fabsf( x - xo ) / w - w * 2.0f;
-		float yc = fabsf( y - yo ) / h - h * 2.0f;
-		float dc = scale * ( qm_math_vector2f_length( QM_MATH_VECTOR2F( 1.0f, 1.0f ) ) - qm_math_vector2f_length( QM_MATH_VECTOR2F( xc, yc ) ) );
+		float vpos = ( yo - y ) / ( h / 2.0f );
+		if ( vpos >= 0.0f )
+		{
+			continue;
+		}
 
 		QmMathColour4ub colour = qm_math_colour4ub( 255, 255, 255, 255 );
 
-		float cw, ch;
-		ape_gui_font_get_character_pixel_size( compassFont, scale / 2.0f, *c, &cw, &ch );
+		float fontScale = -vpos * scale / 4.0f;
 
-		gui_font_draw_character( compassFont, xo - cw / 2.0f, yo - ch, scale / 2.0f, &colour, *c++ );
+		float cw, ch;
+		ape_gui_font_get_character_pixel_size( compassFont, fontScale, *c, &cw, &ch );
+
+		gui_font_draw_character( compassFont, xo - cw / 2.0f, yo - ch * 1.5f, fontScale, &colour, *c );
 
 		PlgImmPushVertex( xo, yo, 0.0f );
 		PlgImmColour( 255, 0, 0, 255 );
