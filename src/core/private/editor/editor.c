600bf8aa9cd2cad3c3497db17fd52f95f36b5975
hogsy <markelswo@gmail.com>
2025-12-28 19:24:59 +0000

    core: fixed some stupidity with drawing quads
    Signed-off-by: hogsy <markelswo@gmail.com>

--- src/core/private/editor/editor.c
+++ src/core/private/editor/editor.c
@@ -924,7 +924,7 @@ static void draw_node_text_overlay( ApeEditorInstance *self, ApeWorldNode *root,
 				colour = QM_MATH_COLOUR4UB( 255, 255, 255, 255 );
 			}
 
-			ape_draw_textured_quad( material, screenPos.x - ( scale / 2.0f ), screenPos.y, scale, -scale, &colour, 0 );
+			ape_draw_textured_quad( material, screenPos.x - ( scale / 2.0f ), screenPos.y - scale, scale, scale, &colour, 0 );
 		}
 	}
 }
